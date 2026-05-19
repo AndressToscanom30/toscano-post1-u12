0a3915a29a81445f827ec161265a73b31045efd4
AndressToscanom30 <andress.toscano@gmail.com>
2026-05-19 13:36:32 -0500

    Resto de dependencias para las pruebas automatizadas

---
+++ src/test/java/com/universidad/libros/LibrosApplicationTests.java
@@ -0,0 +1,12 @@
+package com.universidad.libros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
